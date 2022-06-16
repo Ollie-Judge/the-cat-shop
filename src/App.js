f3bc6650738e55fdb07f6f492e80bc3900e7e444
John Bridges <jbridges373@gmail.com>
2022-06-16 16:06:06 +0100

    data commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Product from "./components/Product";
 
 const App = () => {
-  return (
-    <React.Fragment>
-      <Navbar />
-    </React.Fragment>
-  );
-};
-
+    const [show, setShow] = useState(true);
+    const [cart, setCart] = useState([]);
+  
+    const handleClick = (item) => {
+      if (cart.indexOf(item) !== -1) return;
+      setCart([...cart, item]);
+    };
+  
+    const handleChange = (item, d) => {
+      const ind = cart.indexOf(item);
+      const arr = cart;
+      arr[ind].amount += d;
+  
+      if (arr[ind].amount === 0) arr[ind].amount = 1;
+      setCart([...arr]);
+    };
+  
+     
+    return (
+      <React.Fragment>
+        <Navbar setShow={setShow} size={cart.length} />
+        {show ? (
+          <Product handleClick={handleClick} />
+        ) : (
+          <Basket cart={cart} setCart={setCart} handleChange={handleChange} />
+        )}
+  
+        <Footer />
+      </React.Fragment>
+    );
+  };
+  
 export default App;
