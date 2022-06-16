13ad53e88124046d1ca696f0a2f41989248c8782
Ollie-Judge <ollie.judge@hotmail.com>
2022-06-16 14:45:59 +0100

    added a nav bar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
-import "./App.css";
+import React from "react";
+import Navbar from "./components/Navbar";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <React.Fragment>
+      <Navbar />
+    </React.Fragment>
+  );
+};
 
 export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import "../styling/Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbarContainer">
+      <div>
+        <h2 className="title">Cats 4 Life</h2>
+      </div>
+
+      <nav>
+        <div className="shoppingContainer">
+          <span>Shop</span>
+
+          <div className="shoppingCart">
+            <span>
+              <i class="fas fa-shopping-basket"></i>
+            </span>
+            <span>0</span>
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
