a724dcdfb651376e7dca3d4cb74bd80571bfd7a2
James Lindsay <jamlindsay97@gmail.com>
2022-06-16 15:28:18 +0100

    footer commit 1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <React.Fragment>
       <Navbar />
+      <Footer />
     </React.Fragment>
   );
 };
